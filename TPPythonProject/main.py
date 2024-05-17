0cf08f54586963730e183f0f2dacf14da4c2b92d
Alexander422-3 <166226200+Alexander422-3@users.noreply.github.com>
2024-05-17 17:52:20 +0700

    Update main.py
    Добавил возможность связи с frontend и обработкой запросов

--- TPPythonProject/main.py
+++ TPPythonProject/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api import router as api_router
 from contextlib import asynccontextmanager
 from database import create_tables
@@ -13,3 +14,18 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(api_router)
+
+
+
+origins = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
